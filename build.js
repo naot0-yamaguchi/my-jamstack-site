ae4722081041fe219df2eb6b81cdae3ec494e6a1
naot0-yamaguchi <tech.brain.naoto.yamaguchi@gmail.com>
2024-07-29 01:44:50 +0900

    fix build.js

--- build.js
+++ build.js
@@ -1,26 +1,26 @@
-const fs = require('fs');
-const path = require('path');
-const { marked } = require('marked');
-const Mustache = require('mustache');
+import { readFileSync, writeFileSync, readdirSync } from 'fs';
+import { join } from 'path';
+import { marked } from 'marked';
+import { render } from 'mustache';
 
 const srcDir = './src';
 const publicDir = './public';
 
 const templates = {
-    layout: fs.readFileSync(path.join(srcDir, 'templates', 'layout.html'), 'utf-8'),
-    header: fs.readFileSync(path.join(srcDir, 'templates', 'header.html'), 'utf-8'),
-    footer: fs.readFileSync(path.join(srcDir, 'templates', 'footer.html'), 'utf-8'),
+    layout: readFileSync(join(srcDir, 'templates', 'layout.html'), 'utf-8'),
+    header: readFileSync(join(srcDir, 'templates', 'header.html'), 'utf-8'),
+    footer: readFileSync(join(srcDir, 'templates', 'footer.html'), 'utf-8'),
 };
 
 function buildPage(contentFile) {
-    const contentMarkdown = fs.readFileSync(path.join(srcDir, 'content', contentFile), 'utf-8');
-    const contentHtml = marked(contentMarkdown);
+    const contentMarkdown = readFileSync(join(srcDir, 'content', contentFile), 'utf-8');
+    const contentHtml = marked.parse(contentMarkdown);
     const frontMatterMatch = contentMarkdown.match(/---\n([\s\S]+?)\n---/);
     const frontMatter = frontMatterMatch ? frontMatterMatch[1] : '';
     const titleMatch = frontMatter.match(/title:\s*(.+)/);
     const title = titleMatch ? titleMatch[1] : 'No Title';
 
-    const html = Mustache.render(templates.layout, {
+    const html = render(templates.layout, {
         title,
         content: contentHtml,
     }, {
@@ -29,9 +29,9 @@ function buildPage(contentFile) {
     });
 
     const outputFileName = contentFile.replace('.md', '.html');
-    fs.writeFileSync(path.join(publicDir, outputFileName), html);
+    writeFileSync(join(publicDir, outputFileName), html);
 }
 
-fs.readdirSync(path.join(srcDir, 'content')).forEach(buildPage);
+readdirSync(join(srcDir, 'content')).forEach(buildPage);
 
 console.log('Site built successfully!');
